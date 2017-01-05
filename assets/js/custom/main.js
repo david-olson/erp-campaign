e5360bed89ea06ca84006fa90292233d6b87e9ec
David Olson <david_g_olson@premierinc.com>
2017-01-05 17:37:34 -0500

    Finalize cross browser and device testing

--- assets/js/custom/main.js
+++ assets/js/custom/main.js
@@ -1,13 +1,31 @@
 window.load = main();
 
 window.onresize = function () {
+    document.getElementById('questions').scrollTop = 0;
     resizeIntro();
     resizeQuestions();
 }
 
 
 function main() {
+    
+    var browser = {
+    isIe: function () {
+        return navigator.appVersion.indexOf("MSIE") != -1;
+    },
+    navigator: navigator.appVersion,
+    getVersion: function () {
+        var version = 999; // we assume a sane browser
+        if (navigator.appVersion.indexOf("MSIE") != -1)
+        // bah, IE again, lets downgrade version number
+            version = parseFloat(navigator.appVersion.split("MSIE")[1]);
+        return version;
+    }
+};
+if (browser.isIe() && browser.getVersion() <= 9) {
 
+       alert("You are currently using Internet Explorer " + browser.getVersion() + " or are viewing the site in Compatibility View, please upgrade for a better user experience.")
+    }
     var advanceButtons = document.getElementsByClassName('advance_button'),
         moneyStack = document.getElementsByClassName('money_stack'),
         backButton = document.getElementsByClassName('back_button'),
@@ -18,15 +36,15 @@ function main() {
             advancePanel(this);
         });
     }
-    
+
     for (i = 0; i < moneyStack.length; i++) {
-        moneyStack[i].addEventListener('click', function() {
+        moneyStack[i].addEventListener('click', function () {
             moneyStackClick(this);
         });
     }
-    
-    for (i = 0; i<backButton.length; i++) {
-        backButton[i].addEventListener('click', function() {
+
+    for (i = 0; i < backButton.length; i++) {
+        backButton[i].addEventListener('click', function () {
             reversePanel(this);
         });
     }
@@ -42,6 +60,7 @@ function main() {
         question1Next.style.opacity = 1;
 
     });
+    
 }
 
 function resizeIntro() {
@@ -62,8 +81,12 @@ function resizeIntro() {
         panelHeight = panelContent[i].clientHeight;
         if (panelHeight > viewPortH) {
             panelContent[i].style.position = "static";
-            panelContent[i].style.top = "initial";
-            panelContent[i].style.transform = "initial";
+            panelContent[i].style.top = "0";
+            panelContent[i].style.transform = "translateY(0%)";
+        } else if (panelHeight <= viewPortH) {
+            panelContent[i].style.position = 'absolute';
+            panelContent[i].style.top = '50%';
+            panelContent[i].style.transform = 'translateY(-50%)';
         }
     }
 }
@@ -73,24 +96,37 @@ function resizeQuestions() {
         questionContent = document.getElementsByClassName('question_content'),
         questionHeight,
         viewPortH = window.innerHeight,
+        currentHeight = 0,
         i;
     document.getElementById('questions').style.minHeight = viewPortH + 'px';
-    for (i = 0; i < theQuestions.length; i++) {
-        theQuestions[i].style.minHeight = viewPortH + 'px';
-    }
     for (i = 0; i < questionContent.length; i++) {
         questionHeight = questionContent[i].clientHeight;
         if (questionHeight > viewPortH) {
             questionContent[i].style.position = "static";
-            questionContent[i].style.top = "initial";
-            questionContent[i].style.transform = "initial";
+            questionContent[i].style.top = "0";
+            questionContent[i].style.transform = "translateY(0%)";
         } else if (questionHeight <= viewPortH) {
             questionContent[i].style.position = 'absolute';
             questionContent[i].style.top = '50%';
             questionContent[i].style.transform = 'translateY(-50%)';
-            
         }
     }
+    for (i = 0; i < theQuestions.length; i++) {
+        if (currentHeight < theQuestions[i].clientHeight) {
+            currentHeight = theQuestions[i].clientHeight;
+        }
+    }
+    for (i = 0; i < theQuestions.length; i++) {
+        questionHeight = questionContent[i].clientHeight;
+        if (questionHeight <= viewPortH) {
+            theQuestions[i].style.minHeight = viewPortH + 'px';
+
+        } else if (questionHeight > viewPortH) {
+            theQuestions[i].style.minHeight = currentHeight + 'px';
+            console.log(currentHeight);
+        }
+    }
+
 }
 
 function getStarted(e) {
@@ -118,12 +154,12 @@ function advancePanel(e) {
         nextQuestion = parseInt(thisQuestion) + 1,
         nextQuestionId = 'question' + nextQuestion,
         nextQuestionObject = document.getElementById(nextQuestionId);
-        TweenLite.to(thisQuestionObject, .33, {
-            x: '-100%'
-        });
-        TweenLite.to(nextQuestionObject, .33, {
-            x: '0%'
-        });
+    TweenLite.to(thisQuestionObject, .33, {
+        x: '-100%'
+    });
+    TweenLite.to(nextQuestionObject, .33, {
+        x: '0%'
+    });
 }
 
 function reversePanel(e) {
@@ -144,31 +180,31 @@ function reversePanel(e) {
 
 function moneyStackClick(e) {
     var value = e.id,
-        theNumber, 
+        theNumber,
         questionSlider,
         cash;
     theNumber = value.substr(value.length - 1);
     questionSlider = value.charAt(1);
-    
+
     theNumber = parseInt(theNumber);
     questionSlider = parseInt(questionSlider);
-    
+
     switch (questionSlider) {
-        case 1:
-            slider1.noUiSlider.set(theNumber);
-            break;
-        case 2:
-            slider2.noUiSlider.set(theNumber);
-            break;
-        case 3:
-            slider3.noUiSlider.set(theNumber);
-            break;
-        case 4:
-            slider4.noUiSlider.set(theNumber);
-            break;
-        case 5:
-            slider5.noUiSlider.set(theNumber);
-            break;
+    case 1:
+        slider1.noUiSlider.set(theNumber);
+        break;
+    case 2:
+        slider2.noUiSlider.set(theNumber);
+        break;
+    case 3:
+        slider3.noUiSlider.set(theNumber);
+        break;
+    case 4:
+        slider4.noUiSlider.set(theNumber);
+        break;
+    case 5:
+        slider5.noUiSlider.set(theNumber);
+        break;
     }
     if (theNumber === 5) {
         cash = new Audio('./assets/audio/money.mp3');
@@ -190,3 +226,4 @@ $(function () {
         }
     });
 });
+
